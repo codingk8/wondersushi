a5dd29bbeeeee333ca6eea686b32953fb6128159
codingk8 <codingk8@gmail.com>
2017-12-13 13:51:53 +0100

    Test code offre spéciale

--- routes/index.js
+++ routes/index.js
@@ -10,16 +10,12 @@ const json_products = require('../db/product.json');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  const kei = json_categories.filter(category => category.id_category == 361); 
+  const kei = json_categories.filter(category => category.id_category == 361);
   console.log(kei);
   const kei_products = kei[0].products.map(key => json_products.filter(product => product.id_product == key)[0]);
 
- res.json(kei_products);
+  res.json(kei_products);
 });
-
-/* Ci-dessous, les éléments concernant la route donnés par Alexis dans le chat */
-
-const SUSHISHOP_API_CATEGORIES = 'https://www.sushishop.fr/api/fr/category/';
 
 
 router.all('/webhook', function (req, res, next) {
@@ -29,53 +25,19 @@ router.all('/webhook', function (req, res, next) {
 
     switch (req.body.result.action) {
 
-      /* case 'test':
-      url = 'https://www.sushishop.fr/fr/all-categories';
-      
-        request(url, function(error, response, html){
-          if(!error){
-            var $ = cheerio.load(html);
-            res.json({
-              speech: 'L\'offre spéciale du moment est ...' + $('h1').text(),
-              source: 'webhook'
-            })
-          }
-        });
-        break; */
+      case 'offrespeciale':
 
-      //     case 'menu':
-      //     url = 'https://www.sushishop.fr/api/fr/product/';
+        const kei = json_categories.filter(category => category.id_category == 361);
+        const kei_products = kei[0].products.map(key => json_products.filter(product => product.id_product == key)[0]);
 
-      //       request(url, function(error, response, html){
-      //         if(!error){
-      //           var $ = cheerio.load(html);
-      //       res.json({
-      //         speech: 'L\'offre spéciale du moment est ...' + $('.category-361').text() + 'qui se compose de ...',
-      //         source: 'webhook'
-      //       });
-      //       break;
+        let element;
+        for (let index = 0; index < kei_products.length; index++) {
+          element += kei_products[index].name;
+        }
 
-      //     default:
-      //       res.json({
-      //         speech: 'Je n\'ai pas compris votre question',
-      //         source: 'webhook'
-      //       });
-      //   }
-      // });
-
-
-      case 'menu':
-        //response.json.results[0].formatted_address
-        const url = 'https://www.sushishop.fr/fr/all-categories';
-
-        request(url, function (error, response, html) {
-          if (!error) {
-            var $ = cheerio.load(html);
-            res.json({
-              speech: 'L offre spéciale du moment est ...' + $('.category-361').text() + 'qui se compose de ...',
-              source: 'webhook'
-            });
-          }
+        res.json({
+          speech: 'L offre spéciale du moment est ...' + element,
+          source: 'webhook'
         });
         break;
 
